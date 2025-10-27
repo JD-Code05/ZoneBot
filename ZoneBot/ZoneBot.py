11c726b66b4f25c9def7d3be039c5ba77f831969
John Daniel L. Ubungen <jdubungen805@gmail.com>
2025-10-27 19:03:02 +0800

    Progress 1 ZoneBot added Bot Setup and GoogleApi Library

--- ZoneBot/ZoneBot.py
+++ ZoneBot/ZoneBot.py
@@ -3,4 +3,10 @@ from discord.ext import commands
 import yt_dlp
 import asyncio
 import os
+from googleapiclient.discovery import build
+
+# For the Bot Setup
+# This part is to define the bot and its intents. The Message Content is needed for reading commands.
+intents = discord.Intents.default()
+intents.message_content = True
 
