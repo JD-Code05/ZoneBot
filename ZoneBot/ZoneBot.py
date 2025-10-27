abe17082e8eac14ed41a8bd29265075b7f6267bb
John Daniel L. Ubungen <jdubungen805@gmail.com>
2025-10-27 20:10:54 +0800

    Added Configurations and Bot Setups

--- ZoneBot/ZoneBot.py
+++ ZoneBot/ZoneBot.py
@@ -5,8 +5,17 @@ import asyncio
 import os
 from googleapiclient.discovery import build
 
+#For the Configuration
+# Values to be filled: Discord Bot Token, Youtube API Key, Youtube Channel ID and Notification Channel ID
+
+DISCORD_BOT_TOKEN = #Discord Bot Token Here
+YOUTUBE_API_KEY = #Youtube API Key Here
+YOUTUBE_CHANNEL_ID = #Youtube Channel ID Here
+NOTIFICATION_CHANNEL_ID = #Notificaiton 
+
 # For the Bot Setup
 # This part is to define the bot and its intents. The Message Content is needed for reading commands.
 intents = discord.Intents.default()
 intents.message_content = True
+
 
