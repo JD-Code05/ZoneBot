4c07073c9b688e34c8c63e5be7d2add842d92036
John Daniel L. Ubungen <jdubungen805@gmail.com>
2025-10-27 20:37:54 +0800

    test

--- ZoneBot/ZoneBot.py
+++ ZoneBot/ZoneBot.py
@@ -16,4 +16,5 @@ from googleapiclient.discovery import build
 # For the Bot Setup
 # This part is to define the bot and its intents. The Message Content is needed for reading commands.
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True 
+#test
